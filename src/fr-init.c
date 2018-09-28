685bd7e5e553553e797fab29064eebaa87ef895d
samwhelp <samwhelp@users.noreply.github.com>
2018-09-28 21:59:00 +0800

    force-use-unar v2

--- src/fr-init.c
+++ src/fr-init.c
@@ -378,6 +378,20 @@ register_commands (void)
 }
 
 
+gboolean force_use_unar (void)
+{
+	FILE* fp = fopen("/tmp/force-use-unar", "r");
+	if (fp) {
+		// file exists, then force use unar.
+		fclose(fp);
+		return TRUE;
+	} else {
+		// file doesn't exist
+		return FALSE;
+	}
+}
+
+
 GType
 get_command_type_from_mime_type (const char    *mime_type,
 				 FrCommandCaps  requested_capabilities)
@@ -387,21 +401,23 @@ get_command_type_from_mime_type (const char    *mime_type,
 	if (mime_type == NULL)
 		return 0;
 
-	if (g_ascii_strcasecmp(mime_type, "application/zip") == 0) { // only for zip
-		if (requested_capabilities == FR_COMMAND_CAN_READ_WRITE) { // first call
-			return 0;
-		} else if (requested_capabilities == FR_COMMAND_CAN_READ) { // second call
-			FrRegisteredCommand *command;
-			//FrCommandCaps        capabilities;
-			command = g_ptr_array_index (Registered_Commands, Registered_Commands->len-1); // last command -> FR_TYPE_COMMAND_UNARCHIVER
-			//capabilities = fr_registered_command_get_capabilities (command, mime_type);
+	if (force_use_unar()) { // Check force use unar
+		if (g_ascii_strcasecmp(mime_type, "application/zip") == 0) { // only for zip
+			if (requested_capabilities == FR_COMMAND_CAN_READ_WRITE) { // first call
+				return 0;
+			} else if (requested_capabilities == FR_COMMAND_CAN_READ) { // second call
+				FrRegisteredCommand *command;
+				//FrCommandCaps        capabilities;
+				command = g_ptr_array_index (Registered_Commands, Registered_Commands->len-1); // last command -> FR_TYPE_COMMAND_UNARCHIVER
+				//capabilities = fr_registered_command_get_capabilities (command, mime_type);
 
-			debug (DEBUG_INFO, " command->type: %d ", command->type);
+				debug (DEBUG_INFO, " command->type: %d ", command->type);
 
-			if (command->type == FR_TYPE_COMMAND_UNARCHIVER) { // force use unar
-				return command->type;
+				if (command->type == FR_TYPE_COMMAND_UNARCHIVER) { // force use unar
+					return command->type;
+				}
+
 			}
-
 		}
 	}
 
